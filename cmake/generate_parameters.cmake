b3faf4f0c7b71a7405259fd96f5e1b1ff6e44541
Mikael Mårtensson <mikael.martensson@knowit.se>
2025-04-28 04:50:18 +0000

    Added gitignore and cmake macro for generating parameters

---
+++ cmake/generate_parameters.cmake
@@ -0,0 +1,28 @@
+# Setting this outside of the macro so that the path to the code generators remain independent
+# of where the macro is invoked
+
+find_program(PYTHON3_BIN python3 REQUIRED)
+
+if(NOT TARGET clean-generated-files)
+add_custom_target(clean-generated-files
+    COMMENT "Cleaning up generated calibration files"
+)
+add_dependencies(clean-generated-files clean-generated-files-parameters)
+endif()
+
+macro(generate_parameters NAME GENERATOR CONFIG_FILE OUTPUT_SOURCE OUTPUT_HEADER)
+    message(GEN: ${OUTPUT_HEADER} ${OUTPUT_SOURCE})
+    add_custom_command(
+        OUTPUT ${OUTPUT_HEADER} ${OUTPUT_SOURCE}
+        COMMAND ${PYTHON3_BIN} ${GENERATOR} --config-file ${CONFIG_FILE} --output-source ${OUTPUT_SOURCE} --output-header ${OUTPUT_HEADER}
+        DEPENDS
+        ${CONFIG_FILE}
+        ${GENERATOR}
+    )
+    add_custom_target(${NAME} DEPENDS ${OUTPUT_HEADER} ${OUTPUT_SOURCE})
+    add_custom_target(clean-generated-files-parameters
+        COMMAND rm -f ${OUTPUT_HEADER} ${OUTPUT_SOURCE}
+        COMMENT "Cleaning up files for 'parameters'"
+    )
+    add_dependencies(clean-generated-files clean-generated-files-parameters)
+endmacro(generate_parameters)
