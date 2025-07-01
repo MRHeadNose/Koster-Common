7b2da7ac2175453e1a85e72bc487eb597547066d
Mikael Mårtensson <mikael.martensson@knowit.se>
2025-07-01 12:23:50 +0000

    Change mask name

--- include/koster-common/alarm.h
+++ include/koster-common/alarm.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 #define ALARM_ORIGIN_MASK 0xF000
-#define ALARM_BOARD_NUMBER_MASK 0x0F00
+#define ALARM_ORIGIN_DETAIL_MASK 0x0F00
 #define ALARM_ERROR_ID_MASK 0x00FF
 
 typedef enum { kAlarmOriginUnknown = 0, kAlarmOriginKoster = 0x1000, kAlarmOriginVinga = 0x2000 } alarm_origin_t;
